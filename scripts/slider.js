af1b7eaf85f70594c94d35bff70dd26e85c221b6
Juho Paaso <jpaaso@juho-pro.local>
2014-09-07 20:47:49 +0300

    Responsive single-pager.

---
+++ scripts/slider.js
@@ -0,0 +1,9 @@
+
+var sliderArray = {
+	"We created the world’s first netbank. Now we’re creating the future of banking experience.",
+	"We were giving birth to e-commerce in the Nordics. Now we’re creating the future of shopping experience.", 
+	"We’re placing business, people and creativity into the heart of digital.",
+	"Vision the future with us.",
+	"We will make it happen.",
+ 	"Because we can."
+}
