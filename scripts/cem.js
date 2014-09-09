a15da2a0a233a0b791882b93d52caca893d1baf9
unknown <lehtoanu@WL302504.eu.tieto.com>
2014-09-09 14:18:55 +0300

    fading

--- scripts/cem.js
+++ scripts/cem.js
@@ -17,7 +17,56 @@ $(function() {
   });
 
 
-   
+   // fade
+  var container = $(".wrapper, .job").not('.wrapper:first').not('#jobs-wrapper').fadeTo(0, 0.5); 
+
+  var windowHeight = $(window).height();
+  var viewTop = $(window).scrollTop();
+  var viewBottom = viewTop + windowHeight;
+            
+  var cemFromTop = $('#cem-wrapper').offset().top - viewTop;
+  var cemFromBottom = viewBottom - $('#cem-wrapper').offset().top - $('#cem-wrapper').height();
+  var cemVisible = windowHeight > $('#cem-wrapper').offset().top;
+
+  if( cemVisible ){
+    $('#cem-wrapper').fadeTo(0, 1);
+  }
+
+    
+  $(window).scroll(function() {
+      container.each(function(i) {
+          
+          var windowHeight = $(window).height();
+          var viewTop = $(window).scrollTop();
+          var viewBottom = viewTop + windowHeight;
+          
+          var distanceFromTop = $(this).offset().top - viewTop;
+          var distanceFromBottom = viewBottom - $(this).offset().top - $(this).height();
+          
+
+          if($(this).height() > (windowHeight + 30)) {
+            var visible = true;
+            var allVisible = true;
+          }
+          else{
+            var visible = (distanceFromTop + distanceFromBottom) <= windowHeight;
+            var allVisible = (distanceFromTop * distanceFromBottom) >= 0;
+          }
+          
+         
+          
+          
+          if (visible) {
+              if (allVisible) {
+                  $(this).stop().delay(500).fadeTo(500, 1);
+              } 
+              else {
+              //$(this).stop().delay(500).fadeTo(500, 0.5);
+              }
+      }
+    });
+  });
+   // fade end
 
 
 
